024c98970c6ef792c15bb2709b795499f3b8441f
Cesar Marrujo <marrujoc@gmail.com>
2019-05-27 01:59:37 -0700

    Styling for tablet portrait

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,16 @@
 import { qs, qsa, $on, $delegate } from './utils';
+import { TimelineLite, CSSPlugin, AttrPlugin, Power1 }  from "gsap/all";
 import '../stylesheets/style.scss';
 import '../scripts/hammer.js';
+import { Power0 } from 'gsap';
+
+const plugins = [ CSSPlugin, AttrPlugin ];
+const timeline = new TimelineLite();
+
+timeline.to(".ssm-logo--svg", 5, {
+  ease: Power4.easeOut,
+  width:'100%'
+});
 
 class StudioSupermassive {
   constructor() {
@@ -8,13 +18,13 @@ class StudioSupermassive {
   }
 
   exposeContactForm = () => {
-    const slideout = document.querySelector('.ssm-slideout');
-    const slideoutToggle = document.querySelectorAll('.ssm-slideout--toggle, .ssm-action--link');
+    const slideout = qs('.ssm-slideout');
+    const slideoutToggle = qsa('.ssm-slideout--toggle, .ssm-action--link');
 
     if(slideout && slideoutToggle.length) {
       slideoutToggle.forEach((toggle) => {
         toggle.addEventListener('click', () => {
-          const isActive = (document.querySelector('.ssm-slideout').getAttribute('data-active') === 'false') ? 'true' : 'false';
+          const isActive = (qs('.ssm-slideout').getAttribute('data-active') === 'false') ? 'true' : 'false';
           slideout.setAttribute('data-active', `${isActive}`);
         });
       });
