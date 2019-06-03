13272a23fe909d8928e000d26b5376d57395b930
Cesar Marrujo <marrujoc@gmail.com>
2019-06-02 18:39:10 -0700

    Adds desktop layout/styling

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,8 +9,13 @@ const timeline = new TimelineLite();
 
 timeline.to(".ssm-logo--svg", 5, {
   ease: Power4.easeOut,
-  width:'100%'
-});
+  width: '100%'
+})
+.to(".ssm-mission--wrapper", 5, {
+  ease: Power4.easeOut,
+  opacity: 1,
+  top: '50%'
+},'-=4.5');
 
 class StudioSupermassive {
   constructor() {
@@ -20,12 +25,14 @@ class StudioSupermassive {
   exposeContactForm = () => {
     const slideout = qs('.ssm-slideout');
     const slideoutToggle = qsa('.ssm-slideout--toggle, .ssm-action--link');
+    const ssmLogoWrapper = qs('.ssm-logo--wrapper');
 
     if(slideout && slideoutToggle.length) {
       slideoutToggle.forEach((toggle) => {
         toggle.addEventListener('click', () => {
           const isActive = (qs('.ssm-slideout').getAttribute('data-active') === 'false') ? 'true' : 'false';
           slideout.setAttribute('data-active', `${isActive}`);
+          ssmLogoWrapper.setAttribute('data-active', `${isActive}`);
         });
       });
     }
