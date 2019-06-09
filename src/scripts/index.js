b500c3f8099f00a7cad35461d4b526f0ad8b2562
Cesar Marrujo <marrujoc@gmail.com>
2019-06-09 14:18:12 -0700

    Adds analytics, and parallax efx

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,22 +4,44 @@ import '../stylesheets/style.scss';
 import '../scripts/hammer.js';
 import { Power0 } from 'gsap';
 
-const plugins = [ CSSPlugin, AttrPlugin ];
-const timeline = new TimelineLite();
-
-timeline.to(".ssm-logo--svg", 5, {
-  ease: Power4.easeOut,
-  width: '100%'
-})
-.to(".ssm-mission--wrapper", 5, {
-  ease: Power4.easeOut,
-  opacity: 1,
-  top: '50%'
-},'-=4.5');
-
 class StudioSupermassive {
   constructor() {
     this.exposeContactForm();
+    this.superMassiveIntro();
+    this.superMassiveParallax();
+  }
+
+  superMassiveIntro = () => {
+    const plugins = [ CSSPlugin, AttrPlugin ];
+    const timeline = new TimelineLite();
+
+    timeline.to(".ssm-logo--svg", 5, {
+      ease: Power4.easeOut,
+      width: '100%'
+    })
+    .to(".ssm-mission--wrapper", 4.5, {
+      ease: Power4.easeOut,
+      opacity: 1,
+      top: '50%'
+    },'-=4.5');
+  }
+
+  superMassiveParallax = () => {
+    const ssm = document.querySelector('.ssm');
+    ssm.addEventListener('mousemove', (e) => {
+      this.superMassiveParallaxEngine(e, ".ssm-logo", -30);
+    });
+  }
+  
+  superMassiveParallaxEngine = (e, target, movement) => {
+    const ssm = document.querySelector('.ssm');
+    const relX = e.pageX - ssm.offsetLeft;
+    const relY = e.pageY - ssm.offsetTop;
+  
+    TweenMax.to(target, 1, {
+      x: (relX - ssm.offsetWidth / 2) / ssm.offsetWidth * movement,
+      y: (relY - ssm.offsetHeight / 2) /ssm.offsetHeight * movement
+    });
   }
 
   exposeContactForm = () => {
